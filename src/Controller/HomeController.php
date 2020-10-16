0b0f41d6adabd81748e28a771a014895af13588d
Alexander Volkov <alexander.volkov@takeaway.com>
2020-10-16 23:52:49 +0200

    benchmark class update

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,16 +10,35 @@ class HomeController extends AbstractController
     {
         $data = [
             'insertData' => [
-                'MySQL' => [4, 5, 8, 10, 14, 16, 20],
-                'Mongo' => [5, 5, 7, 9, 12, 13, 15],
-                'Cassandra' => [5, 6, 6, 13, 13, 16, 17],
+                'xAxis' => $this->generateLogarithmicXAxis(20),
+                'data' => [
+                    'MySQL' => $this->generateRandomLogarithmicData(20),
+                    'Mongo' => $this->generateRandomLogarithmicData(20),
+                    'Cassandra' => $this->generateRandomLogarithmicData(20),
+                ]
             ],
             'updateData' => [
-                'MySQL' => [1, 3, 5, 10, 18, 26, 30],
-                'Mongo' => [4, 6, 7, 7, 10, 15, 18],
-                'Cassandra' => [2, 3, 6, 10, 14, 16, 22],
+                'xAxis' => $this->generateLogarithmicXAxis(20),
+                'data' => [
+                    'MySQL' => $this->generateRandomLogarithmicData(20),
+                    'Mongo' => $this->generateRandomLogarithmicData(20),
+                    'Cassandra' => $this->generateRandomLogarithmicData(20),
+                ]
             ],
         ];
         return $this->render('pages/home.html.twig', $data);
+    }
+
+    protected function generateRandomLogarithmicData(int $length): array
+    {
+        return [4, 5, 8, 10, 14, 16, 20];
+    }
+
+    private function generateLogarithmicXAxis(int $length)
+    {
+        $result = [];
+        for ($i = 0; $i < $length; $i++) {
+
+        }
     }
 }
--- src/Service/BenchmarkService.php
+++ src/Service/BenchmarkService.php
@@ -4,20 +4,81 @@ namespace App\Service;
 
 class BenchmarkService
 {
-    private $startTime;
-    public function start(): void
+    const LOGARITHMIC_STRATEGY = 'logarithmic';
+
+    private float $startTime;
+    private float $totalExecutionTime;
+    private int $operationsCount;
+    private string $strategy;
+    private int $currentOperation;
+    private array $operations;
+
+    public function start(int $operationsCount, string $strategy = self::LOGARITHMIC_STRATEGY): void
     {
-        $this->startTime = microtime();
+        $this->setOperationsCount($operationsCount);
+        $this->strategy = $strategy;
+        $this->currentOperation = 0;
+        $this->startTime = microtime(true);
+    }
+
+    public function setOperationsCount(int $operationsCount): void
+    {
+        $this->operationsCount = $operationsCount;
     }
 
     public function operationHasBeenMade()
     {
-
+        if ($this->shouldBeLogged()) {
+            $this->logOperation();
+        }
     }
 
     public function finish(): void
     {
-
+        $this->totalExecutionTime = microtime(true) - $this->startTime;
     }
 
+    public function getTotalExecutionTime(): float
+    {
+        return $this->totalExecutionTime;
+    }
+
+    public function getTotalOperations(): int
+    {
+        return $this->currentOperation;
+    }
+
+    public function getOperationsCount(): int
+    {
+        return $this->operationsCount;
+    }
+
+    private function shouldBeLogged(): bool
+    {
+        switch($this->strategy)
+        {
+            case self::LOGARITHMIC_STRATEGY:
+
+            default:
+                throw new \Exception('Unsupported strategy: ' . $this->strategy);
+        }
+    }
+
+    private function logOperation(): void
+    {
+        $this->currentOperation++;
+        $this->operations[] = [
+            'operation' => $this->currentOperation,
+            'duration' => $this->lastOperationsExecutionTime(),
+        ];
+    }
+
+    private function lastOperationsExecutionTime(): float
+    {
+        if (empty($this->operations)) {
+            return microtime(true) - $this->startTime;
+        } else {
+            return microtime(true) - $this->operations[count($this->operations) - 1]['duration'];
+        }
+    }
 }
