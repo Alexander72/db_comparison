64dbfca8dcb50053ff85080edc8b6a4b1ce7ea18
Alexander Volkov <alexander.volkov@takeaway.com>
2020-10-19 23:44:42 +0200

    added cassandra repository

---
+++ src/Repository/CassandraEntityRepository.php
@@ -0,0 +1,52 @@
+<?php declare(strict_types=1);
+
+namespace App\Repository;
+
+use App\Entity\Entity;
+use App\Repository\Contract\EntityRepository;
+use Cassandra;
+use Cassandra\SimpleStatement;
+
+class CassandraEntityRepository implements EntityRepository
+{
+    private Cassandra\Session $session;
+
+    public function __construct()
+    {
+        $cluster = Cassandra::cluster()
+            ->withContactPoints('cassandra.database')
+            ->build();
+        $this->session = $cluster->connect('db_comparison_test');
+    }
+
+
+    public function insert(array $entity): void
+    {
+        $statement = new SimpleStatement("
+            INSERT INTO route (origin, destination, price, departure) VALUES (
+            '{$entity['origin']}',
+            '{$entity['destination']}',
+            {$entity['price']},
+            '{$entity['departure']}'
+        )
+        ");
+
+        $this->session->execute($statement, null);
+    }
+
+    public function update(Entity $entity): void
+    {
+        // TODO: Implement update() method.
+    }
+
+    public function selectById(int $entityId): Entity
+    {
+        // TODO: Implement selectById() method.
+    }
+
+    public function deleteById(int $entityId): void
+    {
+        // TODO: Implement deleteById() method.
+    }
+
+}
